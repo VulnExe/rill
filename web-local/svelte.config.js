6efaaf8e88771d7a4a4195acb227ea4a5a7ee13c
Jerry <86043641+VulnExe@users.noreply.github.com>
2025-06-05 23:55:34 +0530

    Update svelte.config.js

--- web-local/svelte.config.js
+++ web-local/svelte.config.js
@@ -1,22 +1,12 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      fallback: "index.html",
-    }),
-    // TODO: enable CSP after addressing error in Pylon and Codemirror
-    // csp: {
-    //   directives: {
-    //     "script-src": ["self"],
-    //   },
-    // },
+    adapter: adapter(),
     files: {
       assets: "../web-common/static",
     },
